e7dd5c403f285c51322391315449c5a054c2baf0
JunJongHun <90402926+JunJongHun@users.noreply.github.com>
2022-11-10 19:18:52 +0900

    docs : profile 컴포넌트 추가

---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="profile">
+      <img
+        className="photo"
+        src="https://images.unsplash.com/photo-1491013516836-7db643ee125a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1450&q=80"
+        alt="baby"
+      />
+      <h2 className="title">Jonghun</h2>
+      <h3 className="job">Front-End</h3>
+    </div>
+  );
+};
+
+export default Profile;
