e11777dc131589a4992cb6bb23147d16519f058c
JunJongHun <90402926+JunJongHun@users.noreply.github.com>
2022-11-18 02:51:12 +0900

    객체 업데이트 응용

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppCounter from "./AppCounter";
 import AppProfile from "./AppProfile";
-import AppMentor from "./components/AppMentor";
+import AppMentors from "./components/AppMentors";
 import Card from "./components/Card";
 import CreateFile from "./components/CreateFile";
 import Pointer from "./components/Pointer";
@@ -12,7 +12,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppMentor></AppMentor>
+    <AppMentors></AppMentors>
   </React.StrictMode>
 );
 
