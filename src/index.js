7db48501277f91e1ef4ea636edb27e3c08e0a354
JunJongHun <90402926+JunJongHun@users.noreply.github.com>
2022-11-28 03:15:45 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppCounter from "./AppCounter";
-import AppProfile from "./AppProfile";
-import AppMentors from "./components/AppMentors";
-import Card from "./components/Card";
-import CreateFile from "./components/CreateFile";
-import Pointer from "./components/Pointer";
-import "./index.css";
+
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <AppMentors></AppMentors>
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
